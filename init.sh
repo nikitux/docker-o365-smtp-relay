b05af70819fad980fb95c19fefaeaedac309c45d
LBegnaud <lbegnaud@radersolutions.com>
2019-10-28 12:43:28 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -18,7 +18,7 @@ if [ ! -z "${EMAIL}" ] && [ ! -z "${EMAILPASS}" ]; then
 #    echo "[smtp.gmail.com]:587    ${EMAIL}:${EMAILPASS}" > /etc/postfix/sasl_passwd
     echo "[SMTP.office365.com]:587    ${EMAIL}:${EMAILPASS}" > /etc/postfix/sasl_passwd
     postmap /etc/postfix/sasl_passwd
-    rm /etc/postfix/sasl_passwd
+    #rm /etc/postfix/sasl_passwd
     echo "postfix EMAIL/EMAILPASS combo is setup."
 else
     echo "EMAIL or EMAILPASS not set!"
